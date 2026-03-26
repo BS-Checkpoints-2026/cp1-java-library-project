1fb77579670481a3a6bd9e26fcb2465f5681887a
sSofia-s <ssofiasiq@gmail.com>
2026-03-26 08:53:21 -0300

    feat(Book): adicionado atributos a entidade Book.
    Adicionado as annotations @Data , @Entity , @Id e @GeneratedValue.

--- src/main/java/br/com/library/models/Book.java
+++ src/main/java/br/com/library/models/Book.java
@@ -1,4 +1,20 @@
 package br.com.library.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
 public class Book {
+    @Id @GeneratedValue (strategy = GenerationType.IDENTITY)
+    public Long id;
+    public String title;
+    public Integer year;
+    public Boolean available;
+    public BigDecimal price;
 }
