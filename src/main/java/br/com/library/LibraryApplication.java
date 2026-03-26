356f2736ee4698acb3edd75b3db477be089d91b3
Moisés Barsoti <moises.barsot2@gmail.com>
2026-03-26 09:49:27 -0300

    feat(LibraryApplication): criado o endpoint healthCheack

--- src/main/java/br/com/library/LibraryApplication.java
+++ src/main/java/br/com/library/LibraryApplication.java
@@ -2,12 +2,21 @@ package br.com.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LibraryApplication {
+
+    record HealthStatus(String status, String message){}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
 	}
 
+    @GetMapping("/")
+    public HealthStatus healthCheack() {
+        return new HealthStatus("Ok", "Api is run");
+    }
 }
